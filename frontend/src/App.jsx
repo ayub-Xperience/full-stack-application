46c2ab7796cd03a2de791816148d37d11ab3e2ee
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-18 19:59:25 +0300

    added react quiers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  return <div>hey whatsApp</div>;
+
+ const {data, error, isLoading } =  useQuery({
+    queryFn: () => fetch('http://http//localhost:2000/test').then(res => res.json())
+  })
+  if (isLoading) return <h1>Loading..</h1>;
+  if (error) return <h2>errror</h2>;
+  return <div>{data.name}</div>;
 }
 
 export default App;
