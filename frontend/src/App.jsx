dd43a7cd09daab8d0fca3b8469678f09e48ca996
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-21 15:49:14 +0300

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,10 @@ function App() {
   const { data, error, isLoading } = useQuery({
     queryFn: () =>
       fetch("http://localhost:2000/api/users/").then((res) => res.json()),
-    queryKey: ["users"],
   });
 
   if (isLoading) return <h1>loading...</h1>;
-  if (error) return <h1>error</h1>;
+  if (error) return <h1>error..</h1>;
 
   return (
     <div>{data.user.length > 0 && data.user.map((users) => <h1>{users.name}</h1>)}</div>
