82c16695609a2950b9616a151f9f7ce66bc2d1ad
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-26 03:25:53 +0300

    added register form

--- frontend/src/Pages/auth/RegisterPage.jsx
+++ frontend/src/Pages/auth/RegisterPage.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const RegisterPage = () => {
   return (
    <div className="min-h-screen flex flex-col items-center justify-center bg-background">
-         {/* <div className="absolute inset-0 bg-gradient-to-br from-secondary to-secondary/20 opacity-50"> */}
+         <div className="absolute inset-0 bg-gradient-to-br from-secondary to-secondary/20 opacity-50" />
            <div className="z-10 w-full max-w-md px-4">
              <div className="mb-8 text-center">
                <h1 className="text-3xl font-bold text-foreground">Jion us today</h1>
