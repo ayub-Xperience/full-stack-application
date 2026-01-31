7778d2044279d1cfe8316cba6c2316194af29eeb
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-01-31 23:28:31 +0300

    added dailog section

--- frontend/src/Pages/Dasboard/DashboardPage.jsx
+++ frontend/src/Pages/Dasboard/DashboardPage.jsx
@@ -1,8 +1,11 @@
 import { DashboardHeader } from "@/components/Dashboard/DashboardHeader";
 import { DashboardWelcome } from "@/components/Dashboard/DashboardWelcome";
 import { TaskForm } from "@/components/task/TaskForm";
+import { useState } from "react";
 
 export const DashboardPage = () => {
+  const [showCreateFoarm, setShowCreateFoarm] = useState(false)
+  const [edtingTask, setEditingTask] = useState(null)
   return (
     <div className="min-h-screen bg-background">
       {/* header  */}
