eebddbc684fd9ae47fbcc17aa6b4332f75b2dc4f
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-11 09:44:14 +0300

    Added

--- backend/routes/TaskDelete.js
+++ backend/routes/TaskDelete.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
-import { validate } from '../middleware/validationZod.js'
 import { deleteTask } from '../controllers/todoTask.js'
 const router = express.Router()
 
