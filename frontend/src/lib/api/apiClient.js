acc82cafcc3e900ac706ae407edea23ac506071c
Dev-AyubAbdi <ayubabdiibrahiim@gmail.com>
2026-02-13 17:33:03 +0300

    solve api

--- frontend/src/lib/api/apiClient.js
+++ frontend/src/lib/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 
-const API_URL = "http://localhost:2000/api";
+const API_URL = "https://task-manager-zn33.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
